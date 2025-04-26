7e1e6fc1832802f96e5d709343afd7e81aaabd91
RuanParreira <ruan.parreira@edu.uniube.br>
2025-04-26 20:28:05 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config(); // Adicionado para carregar as variáveis de ambiente
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// Definindo o caminho para os comandos
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`Esse comando em ${filePath} está com "data" ou "execute" ausentes`);
+  }
+}
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Pronto! Login realizado como ${readyClient.user.tag}`);
+});
+client.login(process.env.TOKEN); // Certifique-se de que o TOKEN está carregado no ambiente
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command){
+    console.error("Comando não encontrado");
+    return
+  } 
+  try{
+    await command.execute(interaction);
+  }
+  catch (error){
+    console.error(error);
+    await interaction.reply("Erro ao executar esse comando!");
+  }
+});
